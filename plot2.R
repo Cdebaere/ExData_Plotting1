7c01f07a5973cab33a7f927b39eda63baf1e440c
Charlotte De Baere <charlotte.debaere@hotmail.com>
2016-05-29 17:20:55 +0200

    Change plot1 and adding plot2

--- plot2.R
+++ plot2.R
@@ -8,14 +8,19 @@ consumption <- read.table(file = "./exdata-data-household_power_consumption/hous
 consumption$Date <- as.Date(consumption$Date, format="%d/%m/%Y")
 consumption_filtered <- subset(consumption, Date >= "2007-02-01" & Date <= "2007-02-02")
 
+#Paste date and time
+datetime <- paste(consumption_filtered$Date, consumption_filtered$Time)
+consumption_filtered$Datetime <- strptime(datetime, format="%Y-%m-%d %H:%M:%S")
 
-#construct Plot1
-hist(x=consumption_filtered$Global_active_power, 
-     main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", 
-     ylab="Frequency", 
-     col="Red")
+#construct Plot2
+plot(x=consumption_filtered$Datetime,
+     y=consumption_filtered$Global_active_power,
+     type="l",
+     xlab="",
+     ylab="Global Active Power (kilowatts)")
 
-#Save as plot1.png
-dev.copy(device=png, file="plot1.png", height=480, width=480)
+#Save as plot2.png
+dev.copy(device=png, file="plot2.png", height=480, width=480)
 dev.off()
+
+
